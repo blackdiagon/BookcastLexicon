e16a6a4c73c3de910abc9b0bf2382e5d2523836e
blackdiagon <blackdiagongames@gmail.com>
2024-12-15 16:23:08 +0100

    Projektdateien hinzufügen.

---
+++ BookcastLexicon/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookcastLexicon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            label2.Text = "Bitte gib die Werte wie folgt an:\r\n\r\nBuchtitel-#Folgennummer-Zeitangabe-Schlagwort-Infos-Quellen\r\n\r\nBeispiel:\r\nStein der Weisen - #16 - 1:05:38 - Eule - Hedwig beißt Harry, habe Eulenverhalten analyisert - wikipedia.de/eulen";
+            label2.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Text = "Übergeben";
+            button5.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            textBox1.Multiline = false;
+            label2.Text = "Bitte lösche eine Zeile durch Angabe der folgenden Werte: \n\nBuchtitel-#Folgennummer-Zeitangabe-Schlagwort\n\nBeispiel:\nStein der Weisen - #16 - 1:05:38 - Eule";
+            label2.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Text = "Löschen";
+            button5.Visible = true;
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            label2.Text = "Bitte gib ein Schlagwort ein, nach dem du suchen möchtest, z.B. Eule.";
+            label2.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Text = "Suchen";
+            button5.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            label2.Visible = false;
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            button4.Visible = false;
+            button5.Visible = false;
+            textBox1.Text = string.Empty;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            switch(button5.Text) 
+            {
+                case "Übergeben":
+                    textBox1.Text = Program.CommitValues(splitStrings(textBox1.Text));
+                    break;
+
+                case "Löschen":
+                    textBox1.Text = Program.DeleteValues(splitStrings(textBox1.Text));
+                    break;
+
+                case "Suchen":
+
+                    textBox1.Text = Program.FindValue(textBox1.Text);
+                    break;
+            }
+        }
+
+        private string[] splitStrings(string input) 
+        {
+            return input.Split('-');
+        }
+
+
+    }
+}
---
+++ BookcastLexicon/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookcastLexicon
+{
+    public class Program
+    {
+
+        public static SQLiteConnection sqlite_conn;
+
+        static void Main(string[] args)
+        {
+
+            sqlite_conn = CreateConnection();
+
+            try 
+            {
+                CreateTable();
+            }
+
+            catch { }
+
+            //DropTable(sqlite_conn);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());  // MainForm sollte dein Formular sein
+        }
+
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True; ");
+         try
+            {
+                sqlite_conn.Open();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
+        }
+
+        static void CreateTable()
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE BookcastDB(Buchtitel TEXT, Folgennummer TEXT, Zeitangabe TEXT, Schlagwort TEXT, Infos TEXT, Quellen TEXT)";
+            sqlite_cmd = sqlite_conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+
+        }
+
+        public static string CommitValues(string[] values)
+        {
+            try 
+            {
+                SQLiteCommand sqlite_cmd;
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "INSERT INTO BookcastDB (Buchtitel, Folgennummer, Zeitangabe, Schlagwort, Infos, Quellen) " +
+                                         "VALUES (@Buchtitel, @Folgennummer, @Zeitangabe, @Schlagwort, @Infos, @Quellen)";
+
+                sqlite_cmd.Parameters.AddWithValue("@Buchtitel", values[0].Trim());
+                sqlite_cmd.Parameters.AddWithValue("@Folgennummer", values[1].Trim());
+                sqlite_cmd.Parameters.AddWithValue("@Zeitangabe", values[2].Trim());
+                sqlite_cmd.Parameters.AddWithValue("@Schlagwort", values[3].Trim());
+                sqlite_cmd.Parameters.AddWithValue("@Infos", values[4].Trim());
+                sqlite_cmd.Parameters.AddWithValue("@Quellen", values[5].Trim());
+
+                sqlite_cmd.ExecuteNonQuery();
+                return "Daten erfolgreich übertragen";
+            }
+
+            catch(Exception ex) 
+            {
+                return ex.Message;
+            }
+
+
+        }
+
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM SampleTable";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            conn.Close();
+        }
+
+        static void DropTable(SQLiteConnection conn) 
+        {
+                SQLiteCommand sqlite_cmd;
+
+                string dropTableSQL = "DROP TABLE IF EXISTS BookcastDB";
+
+                sqlite_cmd = conn.CreateCommand();
+                sqlite_cmd.CommandText = dropTableSQL;
+                sqlite_cmd.ExecuteNonQuery();
+
+        }
+
+
+        public static string FindValue(string value) 
+        {
+
+            try
+            {
+
+                SQLiteDataReader sqlite_datareader;
+                SQLiteCommand sqlite_cmd;
+                string output = $"Folgende Informationen sind zum Thema {value} hinterlegt:{Environment.NewLine}{Environment.NewLine}";
+
+                sqlite_cmd = sqlite_conn.CreateCommand();
+                sqlite_cmd.CommandText = "SELECT Buchtitel, Folgennummer, Zeitangabe, Infos, Quellen FROM BookcastDB WHERE Schlagwort = @value";
+                sqlite_cmd.Parameters.AddWithValue("@value", value);
+
+                sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+                int entryNumber = 1;
+                bool found = false; 
+
+                while (sqlite_datareader.Read())
+                {
+                    found = true;
+
+                    string entry = $"---{entryNumber}---{Environment.NewLine}{Environment.NewLine}";
+
+                    string buchtitel = sqlite_datareader.IsDBNull(0) ? "NULL" : sqlite_datareader["Buchtitel"].ToString();
+                    string folgennummer = sqlite_datareader.IsDBNull(1) ? "NULL" : sqlite_datareader["Folgennummer"].ToString();
+                    string zeitangabe = sqlite_datareader.IsDBNull(2) ? "NULL" : sqlite_datareader["Zeitangabe"].ToString();
+                    string infos = sqlite_datareader.IsDBNull(3) ? "NULL" : sqlite_datareader["Infos"].ToString();
+                    string quellen = sqlite_datareader.IsDBNull(4) ? "NULL" : sqlite_datareader["Quellen"].ToString() ;
+
+                    entry += $"{buchtitel}, Folge {folgennummer.Split('#')[1]}, Zeit: {zeitangabe} {Environment.NewLine}{Environment.NewLine}{infos}{Environment.NewLine}{Environment.NewLine}Quellen: {quellen}{Environment.NewLine}{Environment.NewLine}";
+
+                    output += entry;
+
+                    entryNumber++;
+                }
+
+                if (!found)
+                {
+                    return $"Keine Einträge gefunden.";
+                }
+
+                return output;
+            }
+
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+        public static string DeleteValues(string[] values)
+        {
+            try
+            {
+                string sqlQuery = "DELETE FROM BookcastDB WHERE Buchtitel = @Buchtitel AND Folgennummer = @Folgennummer AND Zeitangabe = @Zeitangabe AND Schlagwort = @Schlagwort";
+
+                using (SQLiteCommand sqlite_cmd = new SQLiteCommand(sqlQuery, sqlite_conn))
+                {
+                    sqlite_cmd.Parameters.AddWithValue("@Buchtitel", values[0].Trim());
+                    sqlite_cmd.Parameters.AddWithValue("@Folgennummer", values[1].Trim());
+                    sqlite_cmd.Parameters.AddWithValue("@Zeitangabe", values[2].Trim());
+                    sqlite_cmd.Parameters.AddWithValue("@Schlagwort", values[3].Trim());
+
+                    int rowsAffected = sqlite_cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        return "Die Zeile wurde erfolgreich gelöscht.";
+                    }
+                    else
+                    {
+                        return "Keine Übereinstimmung gefunden. Keine Zeile gelöscht.";
+                    }
+                    }
+      
+            }
+            catch (Exception ex)
+            {
+                return $"Fehler beim Löschen der Zeile: {ex.Message}";
+            }
+        }
+    }
+}
